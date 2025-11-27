b3db88d427ad7d72eb87a1ae7fd964e28d86d430
Aritz Beraza <aritz.beraza@veepee.com>
2025-11-27 17:03:48 +0100

    Add blink for autocompletion

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,10 +1,10 @@
 return {
   { "MeanderingProgrammer/render-markdown.nvim", 
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' },
-    ft = { "markdown", "codecompanion", "avante"}, 
+    ft = { "markdown", "codecompanion", "Avante"}, 
     opts={ 
-      file_types = { "markdown", "codecompanion", "avante" },
-      render_modes = true, 
+      file_types = { "markdown", "codecompanion", "Avante" },
+      render_modes = { 'n', 'c', 't'},  -- normal, command, and terminal modes 
       anti_conceal = {
         above = 0,
         below = 1,
