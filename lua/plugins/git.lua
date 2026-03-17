fd6ce9d5c42e67e471c9223db96c015f0881fa09
Aritz Beraza <aritz.beraza@veepee.com>
2026-03-17 11:33:42 +0100

    feat(lsp): Restart LSP on fugitive branch change

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,18 +1,33 @@
 return {
   { 'tpope/vim-fugitive',
+    enabled = true,
     lazy = true,
     cmd = {
       'G',
       'Git',
     },
-    -- config = function()
-    -- end
+    config = function()
+      local last_head = vim.fn.FugitiveHead()
+
+      vim.api.nvim_create_autocmd('FugitiveChanged', {
+        group = vim.api.nvim_create_augroup('FugitiveLspRestartOnBranchChange', { clear = true }),
+        callback = function()
+          local head = vim.fn.FugitiveHead()
+          if head ~= last_head then
+            last_head = head
+            vim.cmd('LspRestart')
+          end
+        end,
+      })
+    end,
   },
   { 'idanarye/vim-merginal',
+    enabled = true,
     lazy = true,
     cmd = { 'Merginal', 'MerginalToggle' },
   },
   { 'lewis6991/gitsigns.nvim',
+    enabled = true,
     event = { 'BufReadPre', 'BufNewFile' },
     opts = {
       signcolumn = true,
@@ -20,11 +35,10 @@ return {
       current_line_blame = false,
       on_attach = function(bufnr)
 	local gitsigns = require('gitsigns')
-
-	vim.keymap.set('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = 'Toggle Line Blame', noremap = true, silent = true })
-	vim.keymap.set('n', '<leader>hb', function() gitsigns.blame_line{full=true} end, { desc = 'Show Line Blame', noremap = true, silent = true })
-      end
-    }
+      vim.keymap.set('n', '<leader>tb', '<cmd>Gitsigns toggle_current_line_blame<cr>', { buffer = bufnr, desc = 'Toggle Line Blame', noremap = true, silent = true })
+      vim.keymap.set('n', '<leader>tg', '<cmd>Gitsigns toggle_signs<cr>', { buffer = bufnr, desc = 'Toggle GitSigns', noremap = true, silent = true })
+    end
+  }
   }
 
 }
